d9ad5d787f3f07c1f40bce81c186f3d410e0627d
sumit umariya <sumitumariya@gmail.com>
2024-04-30 14:42:49 +0530

    first commit

---
+++ project/app/models.py
@@ -0,0 +1,16 @@
+
+
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    stu_name = models.CharField(max_length=100)
+    stu_email = models.EmailField()
+    stu_city = models.CharField(max_length=50)
+    
+    class Meta:
+        db_table = 'Student'
+        verbose_name_plural = "Student" 
+    
+    def __str__(self):
+        return self.stu_name
---
+++ project/app/views.py
@@ -0,0 +1,18 @@
+
+from .models import Student
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+#    return  HttpResponse ("hello")
+    data =Student.objects.create(
+          stu_name = 'arpit',
+          stu_email = 'arpit@gmail.com',
+          stu_city = 'indore'  
+    )
+    data =Student.objects.get(id=1)       #return single object
+
+    # print(data.id )
+    print(data.stu_name)
+    print(data.stu_email )
+    print(data.city)
+    
