4d0722fde2a2949c0683678dee5287eaf585b933
sumitumariya <sumitumariya11@gmail.com>
2024-06-23 19:58:54 +0530

    views.py

--- project/app/views.py
+++ project/app/views.py
@@ -9,10 +9,10 @@ def home(request):
           stu_email = 'arpit@gmail.com',
           stu_city = 'indore'  
     )
-    data =Student.objects.get(id=1)       #return single object
+    # data =Student.objects.get(id=1)       #return single object
 
     # print(data.id )
-    print(data.stu_name)
-    print(data.stu_email )
-    print(data.city)
+    # print(data.stu_name)
+    # print(data.stu_email )
+    # print(data.city)
     
