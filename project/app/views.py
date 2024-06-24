7e05363bf6e2475cc5dcaf01b15ac5e5d8348fbd
sumitumariya <sumitumariya11@gmail.com>
2024-06-25 02:24:28 +0530

    views.py

--- project/app/views.py
+++ project/app/views.py
@@ -3,16 +3,16 @@ from .models import Student
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-#    return  HttpResponse ("hello")
+   return  HttpResponse ("hello")
     data =Student.objects.create(
           stu_name = 'arpit',
           stu_email = 'arpit@gmail.com',
           stu_city = 'indore'  
     )
-    # data =Student.objects.get(id=1)       #return single object
+    data =Student.objects.get(id=1)       #return single object
 
-    # print(data.id )
-    # print(data.stu_name)
-    # print(data.stu_email )
-    # print(data.city)
+    print(data.id )
+    print(data.stu_name)
+    print(data.stu_email )
+    print(data.city)
     
